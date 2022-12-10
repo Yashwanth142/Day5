7abe15b03fcbe047ff3bc4a108f5e656fdf69042
Yashwanth142 <119283440+Yashwanth142@users.noreply.github.com>
2022-12-10 14:38:00 +0530

    Update unit.sh

--- SequencesPracticeProblems/unit.sh
+++ SequencesPracticeProblems/unit.sh
@@ -1,13 +1,10 @@
-feet=12
-in=$((1/$feet))
-n=$(($in*42))
+n=`awk 'BEGIN{printf("%0.2f",'42' * 0.0833333333)}'`
 echo "the 42in in feet is $n";
 
 rfeet=$((60*40))
-echo $rfeet
-rmeter=$(($rfeet * 0.3048))
+rmeter=`awk 'BEGIN{printf("%0.2f",'$rfeet' * 0.3048)}'`
 echo "Rectangular plot of 60 x 40 feet in meters is $rmeter";
 
 sfeet=43560
-acr=$((25/$sfeet))
+acr=`awk 'BEGIN{printf("%0.2f",'25' * 2.29568411386593200000)}'`
 echo "The area of 25 plots in acres is $acr";
