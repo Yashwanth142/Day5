a7ce9d58ac06a2d24cfd423c49dc44dd7d1d7d7d
Yashwanth142 <119283440+Yashwanth142@users.noreply.github.com>
2022-12-10 14:42:54 +0530

    Update p1.sh

--- SelectionPracticeProblems/ifelse/p1.sh
+++ SelectionPracticeProblems/ifelse/p1.sh
@@ -22,14 +22,55 @@ then
      fi
 elif [[ $r2 -gt $r3 ]]
 then
-    if [[ $r2 -gt $r5]]
+    if [[ $r2 -gt $r5 ]]
     then
         echo "the $r2 is the max number";
+    elif [[ $r3 -gt $r5 ]]
+    then
+        echo "the $r3 is the max number";
     else
-        echo "the $r5 is the max number";
+        echo "the $r5 is max number";
     fi
-else
+elif [[ $r4 -gt $r5 ]]
+then
     echo "the $r4 is the max number";
+else
+    echo "the $r5 is the max number";
 fi
+
+if [[ $r1 -lt $r2 && $r3 -lt $r4 ]]
+then
+     if [[ $r1 -lt $r3 ]]
+     then
+          if [[ $r1 -lt $r5 ]]
+          then
+              echo "the $r1 is the min number";
+          else
+              echo "the $r5 is the min number";
+          fi
+     elif [[ $r3 -lt $r5 ]]
+     then
+          echo "the $r3 is the min number";
+     else
+          echo "the $r5 is the min number";
+     fi
+elif [[ $r2 -lt $r3 ]]
+then
+    if [[ $r2 -lt $r5 ]]
+    then
+        echo "the $r2 is the min number";
+    elif [[ $r3 -lt $r5 ]]
+    then
+        echo "the $r3 is the min number";
+    else
+        echo "the $r5 is min number";
+    fi
+elif [[ $r4 -lt $r5 ]]
+then
+    echo "the $r4 is the min number";
+else
+    echo "the $r5 is the max number";
+fi
+
 
 
